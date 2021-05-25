fdea837d1e0963af93452c2098b65ab038e93c19
Vova <volodya_samsonov@inbox.ru>
2021-05-25 21:59:33 +0300

    Супер последняя версия

--- src/test/java/ru/krjava/event_agency/EventAgencyApplicationTests.java
+++ src/test/java/ru/krjava/event_agency/EventAgencyApplicationTests.java
@@ -1,13 +1,21 @@
 package ru.krjava.event_agency;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.krjava.event_agency.controller.HomeController;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class EventAgencyApplicationTests {
 
+    @Autowired
+    private HomeController controller;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception{
+        assertThat(controller).isNotNull();
     }
 
 }
